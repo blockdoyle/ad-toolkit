bf4b4a162710f85b9d25fd39bad1838e30b21e21
blockdoyle <blockdoyle@gmail.com>
2021-11-14 21:18:36 -0500

    created ShowAll function
    Created a function called 'ShowAll' that will display all users in the AD environment

--- ad-toolkit.ps1
+++ ad-toolkit.ps1
@@ -1,3 +1,24 @@
 $motd = Get-Content .\motd
 $motd
 
+Function MainMenu{
+    "1) Show all users              2) Create User(s)"
+    "3) Delete User(s)              4) Create Group(s)"
+    "5) Delete Group(s)"
+    $c = Read-Host
+    if ($c -eq "1") {
+        ShowAll
+    }
+}
+
+Function ShowAll{
+    $suser = Read-Host "Search user"
+    if ($suser -eq "*"){
+        Get-ADUser -Filter *
+    }
+    else {
+        Get-ADUser -Identity $suser
+    }
+}
+
+MainMenu
